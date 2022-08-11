c3e7bbb4ab21a5c244d55a001fad16f0b3b114ca
Eren Kara <erenkyi0@gmail.com>
2022-08-11 14:03:19 +0300

    exit with code 10 instead of 0 when loa is not detected

--- LostArkLogger/Program.cs
+++ LostArkLogger/Program.cs
@@ -55,11 +55,13 @@ namespace LostArkLogger
             (var region, var installedVersion) = VersionCheck.GetLostArkVersion();
             if (installedVersion == null)
             {
-                MessageBox.Show("Launch Lost Ark before launching logger", "Lost Ark Not Running", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                Environment.Exit(0);
+                MessageBox.Show("Launch Lost Ark before launching logger if you want DPS meter to work.", "Lost Ark Not Running", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Environment.Exit(10);
             }
             else if (region == Region.Unknown)
+            {
                 MessageBox.Show("DPS Meter is out of date.\nDPS Meter might not work until updated.\nCheck Discord/Github for more info.\nFeel free to add a message in the discord informing shalzuth that it's out of data", "Out of date!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
             else if (Properties.Settings.Default.Region != region)
             {
                 Properties.Settings.Default.Region = region;
