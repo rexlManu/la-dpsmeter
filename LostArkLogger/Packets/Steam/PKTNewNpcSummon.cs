f6ab9658598c68b22695be8ce7710d8ea99792f6
Emmanuel Lampe <mail@emmanuel-lampe.de>
2022-09-10 09:57:41 +0200

    fix: new npc summon and status effect

--- LostArkLogger/Packets/Steam/PKTNewNpcSummon.cs
+++ LostArkLogger/Packets/Steam/PKTNewNpcSummon.cs
@@ -6,9 +6,9 @@ namespace LostArkLogger
     {
         public void SteamDecode(BitReader reader)
         {
-            b_0 = reader.ReadByte();
             bytearray_0 = reader.ReadBytes(28);
             OwnerId = reader.ReadUInt64();
+            b_0 = reader.ReadByte();
             bytearray_1 = reader.ReadBytes(3);
             npcStruct = reader.Read<NpcStruct>();
         }
--- LostArkLogger/Packets/Steam/StatusEffectData.cs
+++ LostArkLogger/Packets/Steam/StatusEffectData.cs
@@ -12,7 +12,7 @@ namespace LostArkLogger
             EffectInstanceId = reader.ReadUInt32();
             bytearraylist_0 = reader.ReadList<Byte[]>(7);
             b_1 = reader.ReadByte();
-            SourceId = reader.ReadUInt64();
+            s64_1 = reader.ReadUInt64();
             SkillLevel = reader.ReadByte();
             hasValue = reader.ReadByte();
             if (hasValue == 1)
@@ -20,7 +20,7 @@ namespace LostArkLogger
             u32_0 = reader.ReadUInt32();
             s64_0 = reader.ReadSimpleInt();
             StatusEffectId = reader.ReadUInt32();
-            s64_1 = reader.ReadUInt64();
+            SourceId = reader.ReadUInt64();
         }
     }
 }
