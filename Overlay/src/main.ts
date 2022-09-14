e4a625766115418d2611fdece8d9cb4e2b6c6492
Emmanuel Lampe <mail@emmanuel-lampe.de>
2022-09-14 20:05:21 +0200

    fix(overlay): dark mode for overlay

--- Overlay/src/main.ts
+++ Overlay/src/main.ts
@@ -6,6 +6,7 @@ import 'quasar/dist/quasar.css';
 
 import { createPinia } from 'pinia';
 import { Quasar } from 'quasar';
+import { configure } from 'quasar/wrappers';
 import { createApp } from 'vue';
 
 import App from './App.vue';
@@ -13,9 +14,28 @@ import router from './router';
 
 const app = createApp(App);
 
-app.use(Quasar, {
-  plugins: {},
-});
+app.use(
+  Quasar,
+  configure({
+    plugins: {},
+    config: {
+      brand: {
+        primary: "#1976d2",
+        secondary: "#26A69A",
+        accent: "#9C27B0",
+
+        dark: "#1d1d1d",
+        "dark-page": "#121212",
+
+        positive: "#21BA45",
+        negative: "#C10015",
+        info: "#31CCEC",
+        warning: "#F2C037",
+      },
+      dark: true,
+    },
+  })
+);
 app.use(createPinia());
 app.use(router);
 
